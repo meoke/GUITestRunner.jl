1a6d76a05b407acf3bfe13e08c1395f98b7241e9
Paulina Hyzy <paulinahyzy@gmail.com>
2015-11-26 18:42:25 +0100

    Displaying checkobx with facts in the window

--- src/GUITestRunner.jl
+++ src/GUITestRunner.jl
@@ -2,6 +2,25 @@ module GUITestRunner
 using Tk
 using TestRunner
 
+# type TestRunnerWindow
+#   mainWindow
+#   mainFrame
+#   factsWidgets
+#   widgets::Array{Tk.Widget}
+# end
+#
+# function CreateWindow()
+#   w::TestRunnerWindow
+#   w.mainWindow = TK.Toplevel("Julia Test Runner", 350, 600)
+#   pack_stop_propagate(w)
+#
+#   w.mainFrame = Frame(w.mainWindow, padding = [3,3,2,2])
+#   pack(w.mainFrame, expand = true, fill = "both")
+#
+#   l = Label(w.mainFrame, "Specify file with your tests:")
+#   grid(l, 1,1)
+# end
+
 function CreateMainWindow()
   widgets = Array(Tk.Widget, 0)
   testsCheckbuttons = Array(Tk.Widget,0)
@@ -21,33 +40,49 @@ function CreateMainWindow()
   readFileBtn = Button(f, "Load tests!")
   formlayout(readFileBtn, nothing)
 
-  for i in 1:10
-    cb = Checkbutton(f, "Test$i")
-    formlayout(cb, nothing)
-  end
-
-  bind(readFileBtn, "command", load_tests)
+  # for i in 1:10
+  #   cb = Checkbutton(f, "Test$i")
+  #   formlayout(cb, nothing)
+  # end
+  print(typeof(f))
+  bind(readFileBtn, "command", (x)->load_tests(x, f, widgets))
 end
 
-function printFactsNode(testNode)
+function printFactsNode_console(testNode)
   println(testNode.name)
   for i in 1:length(testNode.childs)
     if(isa(testNode.childs[i], FactNode))
       println(testNode.childs[i].name)
     else
       for j in 1:length(testNode.childs[i].childs)
-        printFactsNode(testNode.childs[i].childs[j])
+        printFactsNode_console(testNode.childs[i].childs[j])
       end
     end
   end
 end
 
-function load_tests(path)
+function addFactsNode(testNode, frame, widgets)
+  cb = Checkbutton(frame, testNode.name)
+  formlayout(cb, nothing)
+  for i in 1:length(testNode.childs)
+    if(isa(testNode.childs[i], FactNode))
+      cb = Checkbutton(frame, testNode.childs[i].name)
+      formlayout(cb, nothing)
+    else
+      for j in 1:length(testNode.childs[i].childs)
+        addFactsNode(testNode.childs[i].childs[j], f, widgets)
+      end
+    end
+  end
+end
+
+function load_tests(path,f, widgets)
     #val = get_value(fileNameInput)
     val = "/home/student/.julia/v0.4/GUITestRunner/test/sampleTests.jl"
     testStructure::Vector{TestStructureNode} = TestRunner.get_tests_structure(val)
     for i in 1:length(testStructure)
-      printFactsNode(testStructure[i])
+      printFactsNode_console(testStructure[i])
+      addFactsNode(testStructure[i], f, widgets)
     end
 end
 
--- test/sampleTests.jl
+++ test/sampleTests.jl
@@ -10,10 +10,10 @@ end
 
 facts("Second facts group") do
   @fact 1 --> 1 "Second group first test"
-  @fact 2 --> 2 #Nameless fact
+  #@fact 2 --> 2 #Nameless fact
 end
 
-facts() do #Nameless group
-  @fact 1 --> 1 #Nameless fact
-  @fact 2 --> 1 #Failing nameless fact in nameless group
-end
+# facts() do #Nameless group
+#   @fact 1 --> 1 #Nameless fact
+#   @fact 2 --> 1 #Failing nameless fact in nameless group
+# end
