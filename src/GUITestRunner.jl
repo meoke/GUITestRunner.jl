799ffc7182217a261ed22f3c5a267b782a60179d
Paulina Hyzy <paulinahyzy@gmail.com>
2015-12-09 02:43:53 +0100

    Refactoring of the callback function for loading tests button.

--- src/GUITestRunner.jl
+++ src/GUITestRunner.jl
@@ -2,25 +2,6 @@ module GUITestRunner
 using Tk
 using TestRunner
 
-# type TestRunnerWindow
-#   mainWindow
-#   mainFrame
-#   factsWidgets
-#   widgets::Array{Tk.Widget}
-# end
-#
-# function CreateWindow()
-#   w::TestRunnerWindow
-#   w.mainWindow = TK.Toplevel("Julia Test Runner", 350, 600)
-#   pack_stop_propagate(w)
-#
-#   w.mainFrame = Frame(w.mainWindow, padding = [3,3,2,2])
-#   pack(w.mainFrame, expand = true, fill = "both")
-#
-#   l = Label(w.mainFrame, "Specify file with your tests:")
-#   grid(l, 1,1)
-# end
-
 function CreateMainWindow()
   widgets = Array(Tk.Widget, 0)
   testsCheckbuttons = Array(Tk.Widget,0)
@@ -37,53 +18,39 @@ function CreateMainWindow()
   fileNameInput = Entry(f)
   grid(fileNameInput, 1, 2)
 
-  readFileBtn = Button(f, "Load tests!")
-  formlayout(readFileBtn, nothing)
+  load_tests_button = Button(f, "Load tests!")
+  formlayout(load_tests_button, nothing)
 
-  # for i in 1:10
-  #   cb = Checkbutton(f, "Test$i")
-  #   formlayout(cb, nothing)
-  # end
-  print(typeof(f))
-  bind(readFileBtn, "command", (x)->load_tests(x, f, widgets))
+  #for testing
+  set_value(fileNameInput, "/home/student/.julia/v0.4/GUITestRunner/test/sampleTests.jl")
+  bind(load_tests_button, "command", (x)->load_tests_callback(x, f, ()->get_value(fileNameInput)))
 end
 
-function printFactsNode_console(testNode)
-  println(testNode.name)
-  for i in 1:length(testNode.childs)
-    if(isa(testNode.childs[i], FactNode))
-      println(testNode.childs[i].name)
-    else
-      for j in 1:length(testNode.childs[i].childs)
-        printFactsNode_console(testNode.childs[i].childs[j])
-      end
-    end
-  end
-end
-
-function addFactsNode(testNode, frame, widgets)
+function addFactsNode(testNode::TestStructureNode, frame::Tk.Tk_Frame)
   cb = Checkbutton(frame, testNode.name)
   formlayout(cb, nothing)
-  for i in 1:length(testNode.childs)
-    if(isa(testNode.childs[i], FactNode))
-      cb = Checkbutton(frame, testNode.childs[i].name)
-      formlayout(cb, nothing)
-    else
-      for j in 1:length(testNode.childs[i].childs)
-        addFactsNode(testNode.childs[i].childs[j], f, widgets)
-      end
-    end
+  for child in TestRunner.children(testNode)
+    addFactsNode(child, frame)
   end
 end
 
-function load_tests(path,f, widgets)
-    #val = get_value(fileNameInput)
-    val = "/home/student/.julia/v0.4/GUITestRunner/test/sampleTests.jl"
-    testStructure::Vector{TestStructureNode} = TestRunner.get_tests_structure(val)
-    for i in 1:length(testStructure)
-      printFactsNode_console(testStructure[i])
-      addFactsNode(testStructure[i], f, widgets)
-    end
+function load_tests_callback(not_used, frame::Tk.Tk_Frame, file_name_func::Function)
+    file_name = file_name_func()
+    testStructure = get_tests_structure(file_name)
+    clean_old_test(frame)
+    display_tests(testStructure, frame)
+end
+
+function display_tests(testStructure::Vector{TestStructureNode}, frame::Tk.Tk_Frame)
+  for node in testStructure
+    addFactsNode(node, frame)
+  end
+end
+
+function clean_old_test(frame::Tk.Tk_Frame)
+  for child in Tk.children(frame)
+    dump(child)
+  end
 end
 
 end # module
