b59119610daba11a1cf4eed4840e38208d580de8
Paulina Hyzy <paulinahyzy@gmail.com>
2015-12-19 19:48:23 +0100

    Refreshing tests list when group of tests are (un)collapsing them.

--- src/GUITestRunner.jl
+++ src/GUITestRunner.jl
@@ -13,28 +13,48 @@ function CreateMainWindow()
   w = Tk.Toplevel("Julia Test Runner", 350, 600)
   pack_stop_propagate(w)
 
-  f = Frame(w, padding = [3,3,2,2])
-  pack(f, expand = true, fill = "both")
+  frame = Frame(w, padding = [3,3,2,2])
+  pack(frame, expand = true, fill = "both")
 
-
-  fileNameInput = Entry(f)
+  fileNameInput = Entry(frame)
 
   grid(fileNameInput, 1, 1)
 
-  browse_dir_button = Button(f, "...")
+  browse_dir_button = Button(frame, "...")
   grid(browse_dir_button, 1, 2)
-  bind(browse_dir_button, "command", (x)->browse_dir_callback(x, ()->fileNameInput))
+  bind(browse_dir_button, "command", (x)->browse_dir_callback(x, fileNameInput))
 
-  load_tests_button = Button(f, "Load tests!")
+  load_tests_button = Button(frame, "Load tests!")
   grid(load_tests_button, 3, 2)
-
-  bind(load_tests_button, "command", (x)->load_tests_callback(x, f, ()->get_value(fileNameInput), ()->Tk.children(f)))
+  Tk.update(w)
+  #for testing
+  set_value(fileNameInput, "/home/student/.julia/v0.4/GUITestRunner/test/sampleTests.jl")
+  bind(load_tests_button, "command", (x)->load_tests_callback(x, frame, ()->get_value(fileNameInput)))
 end
 
-function browse_dir_callback(not_used, get_input_func)
+function browse_dir_callback(not_used, file_name_input)
   choice = GetOpenFile()
-  file_name_input = get_input_func()
   set_value(file_name_input, choice)
+end
+
+function load_tests_callback(not_used, frame::Tk.Tk_Frame, file_name_func::Function)
+    file_name = file_name_func()
+    global tests_structure = get_tests_structure(file_name)
+    clean_old_test(frame.children)
+    display_tests(tests_structure, frame)
+end
+
+function tests_header_callback(not_used, frame_children_func::Function, testNode::TestStructureNode, frame_func::Function)
+  global hidden_tests_groups_names
+  if testNode.name in hidden_tests_groups_names
+    hidden_tests_groups_names = filter(x -> x != testNode.name, hidden_tests_groups_names)
+  else
+    push!(hidden_tests_groups_names, testNode.name)
+  end
+  children = frame_children_func()
+  frame = frame_func()
+  clean_old_test(children)
+  display_tests(tests_structure, frame)
 end
 
 function draw_node(testNode::TestStructureNode, frame::Tk.Tk_Frame)
@@ -51,9 +71,9 @@ function draw_node(testNode::TestStructureNode, frame::Tk.Tk_Frame)
     node_button = Button(frame, text=testNode.name, image=img, compound="left")
   else
     node_button = Button(frame, text=testNode.name, compound="left")
-    bind(node_button, "command", (x)->tests_header_callback(x, ()->Tk.children(frame), testNode, frame))
+    bind(node_button, "command", (x)->tests_header_callback(x, ()->Tk.children(frame), testNode, ()->frame))
   end
-  #push!(tests_nodes, node_button)
+  push!(tests_nodes, node_button)
   Tk.get_path(node_button)
   formlayout(node_button, nothing)
 end
@@ -68,25 +88,7 @@ function add_facts_node(testNode::TestStructureNode, frame::Tk.Tk_Frame)
   end
 end
 
-function load_tests_callback(not_used, frame::Tk.Tk_Frame, file_name_func::Function, frame_children_func::Function)
-    file_name = file_name_func()
-    global tests_structure = get_tests_structure(file_name)
-    children = frame_children_func()
-    clean_old_test(children, frame)
-    display_tests(tests_structure, frame)
-end
 
-function tests_header_callback(not_used, frame_children_func::Function, testNode::TestStructureNode, frame::Tk.Tk_Frame)
-  global hidden_tests_groups_names
-  if testNode.name in hidden_tests_groups_names
-    hidden_tests_groups_names = filter(x -> x != testNode.name, hidden_tests_groups_names)
-  else
-    push!(hidden_tests_groups_names, testNode.name)
-  end
-  children = frame_children_func()
-  clean_old_test(children, frame)
-  display_tests(tests_structure, frame)
-end
 
 function display_tests(testStructure::Vector{TestStructureNode}, frame::Tk.Tk_Frame)
   for node in testStructure
@@ -94,15 +96,12 @@ function display_tests(testStructure::Vector{TestStructureNode}, frame::Tk.Tk_Fr
   end
 end
 
-function clean_old_test(children,frame::Tk.Tk_Frame)
-  global tests_nodes
-  for child in children
-    if isa(child, Tk.Tk_Button) && child in tests_nodes
-      idx = findfirst(tests_nodes, child)
-      deleteat!(tests_nodes, idx)
-      Tk.forget(frame, child)
-    end
+function clean_old_test(children)
+  childrenToRemove = filter(x -> x in tests_nodes, children)
+  for child in childrenToRemove
+    forget(child)
   end
+  empty!(tests_nodes)
 end
 
 end # module
