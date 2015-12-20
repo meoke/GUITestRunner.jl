1067bce82114e9e2a4972ad6413d17f778dfc745
Paulina Hyzy <paulinahyzy@gmail.com>
2015-12-20 16:30:43 +0100

    All displayed tests enclosed in a Tk.Tk_Labelframe

--- src/GUITestRunner.jl
+++ src/GUITestRunner.jl
@@ -3,33 +3,36 @@ using Tk
 using TestRunner
 
 hidden_tests_groups_names = Array(AbstractString, 0)
+
+function run()
 tests_structure = Array(TestStructureNode, 0)
-tests_nodes = Array(Tk.Tk_Widget,0)
+frame = create_main_window(tests_structure)
+end
 
-function CreateMainWindow()
-  widgets = Array(Tk.Widget, 0)
-  testsCheckbuttons = Array(Tk.Widget,0)
+function create_main_window(tests_structure::Array{TestStructureNode})
+  window = Tk.Toplevel("Julia Test Runner", 350, 600)
+  pack_stop_propagate(window)
 
-  w = Tk.Toplevel("Julia Test Runner", 350, 600)
-  pack_stop_propagate(w)
-
-  frame = Frame(w, padding = [3,3,2,2])
+  frame = Frame(window, padding = [3,3,2,2])
   pack(frame, expand = true, fill = "both")
 
-  fileNameInput = Entry(frame)
-
-  grid(fileNameInput, 1, 1)
+  file_name_input = Entry(frame)
+  pack(file_name_input)
 
   browse_dir_button = Button(frame, "...")
-  grid(browse_dir_button, 1, 2)
-  bind(browse_dir_button, "command", (x)->browse_dir_callback(x, fileNameInput))
+  pack(browse_dir_button)
+  bind(browse_dir_button, "command", (x)->browse_dir_callback(x, file_name_input))
 
   load_tests_button = Button(frame, "Load tests!")
-  grid(load_tests_button, 3, 2)
-  Tk.update(w)
-  #for testing
-  set_value(fileNameInput, "/home/student/.julia/v0.4/GUITestRunner/test/sampleTests.jl")
-  bind(load_tests_button, "command", (x)->load_tests_callback(x, frame, ()->get_value(fileNameInput)))
+  pack(load_tests_button)
+  bind(load_tests_button, "command", (x)->load_tests_callback(x, frame, ()->get_value(file_name_input), tests_structure))
+
+  #set values for testing
+  set_value(file_name_input, "/home/student/.julia/v0.4/GUITestRunner/test/sampleTests.jl")
+
+  tests_list_frame = Labelframe(frame, "Tests")
+  pack(tests_list_frame)
+  frame
 end
 
 function browse_dir_callback(not_used, file_name_input)
@@ -37,27 +40,31 @@ function browse_dir_callback(not_used, file_name_input)
   set_value(file_name_input, choice)
 end
 
-function load_tests_callback(not_used, frame::Tk.Tk_Frame, file_name_func::Function)
+function load_tests_callback(not_used, frame::Tk.Tk_Frame, file_name_func::Function, tests_structure)
     file_name = file_name_func()
-    global tests_structure = get_tests_structure(file_name)
-    clean_old_test(frame.children)
-    display_tests(tests_structure, frame)
+    tests_structure = get_tests_structure(file_name)
+    frame_for_tests = get_frame_for_tests(frame)
+    reset_hidden_tests()
+    clear_old_test(frame_for_tests)
+    display_all_tests(tests_structure, frame_for_tests)
 end
 
-function tests_header_callback(not_used, frame_children_func::Function, testNode::TestStructureNode, frame_func::Function)
+function tests_header_callback(not_used, testNode::TestStructureNode, frame::Tk.Tk_Labelframe, tests_structure::Vector{TestStructureNode})
   global hidden_tests_groups_names
   if testNode.name in hidden_tests_groups_names
     hidden_tests_groups_names = filter(x -> x != testNode.name, hidden_tests_groups_names)
   else
     push!(hidden_tests_groups_names, testNode.name)
   end
-  children = frame_children_func()
-  frame = frame_func()
-  clean_old_test(children)
-  display_tests(tests_structure, frame)
+  clear_old_test(frame)
+  display_all_tests(tests_structure, frame)
 end
 
-function draw_node(testNode::TestStructureNode, frame::Tk.Tk_Frame)
+function reset_hidden_tests()
+  empty!(hidden_tests_groups_names)
+end
+
+function CreateButtonForTest(testNode::Union{FactsCollectionNode, FactNode}, frame::Tk.Tk_Labelframe, tests_structure::Vector{TestStructureNode})
   if :result in fieldnames(testNode)
     if testNode.result == true
       img_name = "success.png"
@@ -71,37 +78,42 @@ function draw_node(testNode::TestStructureNode, frame::Tk.Tk_Frame)
     node_button = Button(frame, text=testNode.name, image=img, compound="left")
   else
     node_button = Button(frame, text=testNode.name, compound="left")
-    bind(node_button, "command", (x)->tests_header_callback(x, ()->Tk.children(frame), testNode, ()->frame))
+    bind(node_button, "command", (x)->tests_header_callback(x, testNode, frame, tests_structure))
   end
-  push!(tests_nodes, node_button)
-  Tk.get_path(node_button)
-  formlayout(node_button, nothing)
+  node_button
 end
 
-function add_facts_node(testNode::TestStructureNode, frame::Tk.Tk_Frame)
-  draw_node(testNode, frame)
+function draw_node(testNode::TestStructureNode, frame::Tk.Tk_Labelframe, tests_structure::Vector{TestStructureNode})
+  node_button = CreateButtonForTest(testNode, frame, tests_structure)
+  pack(node_button)
+end
+
+function display_facts_node(testNode::TestStructureNode, frame::Tk.Tk_Labelframe, tests_structure::Vector{TestStructureNode})
+  draw_node(testNode, frame, tests_structure)
   if testNode.name in hidden_tests_groups_names
     return
   end
   for child in TestRunner.children(testNode)
-    add_facts_node(child, frame)
+    display_facts_node(child, frame, tests_structure)
   end
 end
 
-
-
-function display_tests(testStructure::Vector{TestStructureNode}, frame::Tk.Tk_Frame)
+function display_all_tests(testStructure::Vector{TestStructureNode}, frame::Tk.Tk_Labelframe)
   for node in testStructure
-    add_facts_node(node, frame)
+    display_facts_node(node, frame, testStructure)
   end
 end
 
-function clean_old_test(children)
-  childrenToRemove = filter(x -> x in tests_nodes, children)
-  for child in childrenToRemove
-    forget(child)
+function clear_old_test(frame::Tk.Tk_Labelframe)
+  tests_to_remove = frame.children
+  for test in tests_to_remove
+    forget(test)
   end
-  empty!(tests_nodes)
+  empty!(tests_to_remove)
+end
+
+function get_frame_for_tests(frame::Tk.Tk_Frame)
+  filter(x->isa(x, Tk.Tk_Labelframe), frame.children)[1]
 end
 
 end # module
