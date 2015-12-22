17802dd78778e1051c895e6fcc4a6f3630b835b0
Paulina Hyzy <paulinahyzy@gmail.com>
2015-12-23 00:28:14 +0100

    Test file updated

--- test/sampleTests.jl
+++ test/sampleTests.jl
@@ -5,12 +5,14 @@ using FactCheck
 
 facts("First facts group") do
   @fact 1 --> 1 "First group first test"
-  @fact 1 --> 1 "First group second failing test"
+  @fact 1 --> 2 "First group second failing test"
 end
 
 facts("Second facts group") do
   @fact 1 --> 1 "Second group first test"
-  @fact 1 --> 0 "Second group second test"
+  @fact 1 --> 1 "Second group second test"
+  @fact error("test") --> 1 "Error"
+  @fact 1-->2 "a"
   #@fact 2 --> 2 #Nameless fact
 end
 
