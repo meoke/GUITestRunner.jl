0861be6ba035597b08cfa1a2bf611cc226135542
Grzegorz Dziadkiewicz <gdziadkiewicz@future-processing.com>
2015-12-23 15:04:14 +0100

    Added test for linux

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,13 @@ not_throw(_) = true
 facts("Helper functions tests") do
   context("get_image") do
 	@fact_throws ErrorException GUITestRunner.get_image(:pies) 
+	@windows? (
+         begin
+             @pending GUITestRunner.get_image(:question) --> not_throw
+         end
+       : begin
+             @fact GUITestRunner.get_image(:question) --> not_throw
+         end
+       )
   end
 end
