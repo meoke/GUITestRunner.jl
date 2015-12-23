52839e4acd80e2b9b6d6ac9882f87726f50604de
Grzegorz Dziadkiewicz <gdziadkiewicz@future-processing.com>
2015-12-23 15:11:42 +0100

    Added full tests for icons

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,15 +4,19 @@ using FactCheck
 not_throw(_) = true
 
 facts("Helper functions tests") do
-  context("get_image") do
-	@fact_throws ErrorException GUITestRunner.get_image(:pies) 
+  context("geting test buttons icons") do
 	@windows? (
          begin
-             @pending GUITestRunner.get_image(:question) --> not_throw
+            @fact GUITestRunner.get_image(:success) --> not_throw
+            @fact GUITestRunner.get_image(:question)--> not_throw
+			@fact GUITestRunner.get_image(:failure) --> not_throw
          end
        : begin
-             @fact GUITestRunner.get_image(:question) --> not_throw
+			@fact GUITestRunner.get_image(:success) --> not_throw
+            @fact GUITestRunner.get_image(:question)--> not_throw
+			@fact GUITestRunner.get_image(:failure) --> not_throw
          end
        )
+	@fact_throws ErrorException GUITestRunner.get_image(:pies) 
   end
 end
